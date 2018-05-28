2fe376732d1615f778fffc4155dcd456ac5564b1
ftorto <dev.torto@gmail.com>
2018-05-28 11:20:09 +0200

    logfile
    Signed-off-by: ftorto <dev.torto@gmail.com>

--- syncRepo.sh
+++ syncRepo.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+LOGFILE=${DIR}/syncGitRepo.log
+
 syncThis(){
   src_remote=$1
   dest_remote=$2
   echo "Syncing ${src_remote} -> ${dest_remote}"
 
+  date +"%Y-%m-%d %H:%M:%S" >> ${LOGFILE}
+  echo "Syncing ${src_remote} -> ${dest_remote}" >> ${LOGFILE}
+
   wpath=$(mktemp -d)
   pushd $wpath > /dev/null 2>&1
 
-  git clone --mirror ${src_remote} > /dev/null 2>&1
+  git clone --mirror ${src_remote} >> ${LOGFILE} 2>&1
   cd * > /dev/null 2>&1
-  git push --mirror ${dest_remote}
+  git push -f --mirror ${dest_remote} >> ${LOGFILE} 2>&1
 
   popd > /dev/null 2>&1
   rm -rf ${wpath} > /dev/null
   echo
+  echo "---" >> ${LOGFILE}
 }
 
 # Main
